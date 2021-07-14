ec199d1e3f79774e3b1465ac49f04186b4bb0d24
waiaan <waiaan@163.com>
2021-07-14 10:20:07 +0800

    update

--- demo/src/main/java/com/example/service/impl/EmployeeServiceImpl.java
+++ demo/src/main/java/com/example/service/impl/EmployeeServiceImpl.java
@@ -1,54 +1,23 @@
 package com.example.service.impl;
 
-import java.util.Date;
-
 import com.example.dao.EmployeeDao;
 import com.example.service.EmployeeService;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmployeeServiceImpl implements EmployeeService {
-  private String name;
-  private Integer age;
-  private Date birth;
 
-  public EmployeeServiceImpl(String name, Integer age, Date birth) {
-    this.name = name;
-    this.age = age;
-    this.birth = birth;
-  }
-
-  @Override
-  public String toString() {
-    return "EmployeeServiceImpl [age=" + age + ", birth=" + birth + ", name=" + name + "]";
-  }
+  private EmployeeDao ed;
 
   @Override
   public void get() {
-    // TODO Auto-generated method stub
-
+    System.out.println("service impl get method");
+    ed.get();
   }
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
-  public void setAge(Integer age) {
-    this.age = age;
-  }
-
-  public Date getBirth() {
-    return birth;
-  }
-
-  public void setBirth(Date birth) {
-    this.birth = birth;
+  public EmployeeServiceImpl() {
+    System.out.println("service impl constructor");
   }
 
 }
--- demo/src/main/java/com/example/ui/App.java
+++ demo/src/main/java/com/example/ui/App.java
@@ -1,18 +1,16 @@
 package com.example.ui;
 
-import com.example.factory.BeanFactory;
 import com.example.service.EmployeeService;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
   public static void main(String[] args) {
     ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-    EmployeeService es = (EmployeeService) ac.getBean("employeeService");
+    EmployeeService es = (EmployeeService) ac.getBean("employeeServiceImpl");
     System.out.println(es);
     // es.get();
-    // ac.close();
+    ac.close();
     ;
   }
 }
