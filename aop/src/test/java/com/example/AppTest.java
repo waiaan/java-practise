9dd4390eb85f943e3d8edb396d32d607a4efdf71
waiaan <waiaan@163.com>
2021-07-16 17:37:53 +0800

    update

--- aop/src/test/java/com/example/AppTest.java
+++ aop/src/test/java/com/example/AppTest.java
@@ -1,20 +1,24 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import com.example.service.EmployeeService;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        EmployeeService es = (EmployeeService) ac.getBean("employeeService");
+        es.delete(100);
+        es.save();
+        es.update(10);
     }
 }
