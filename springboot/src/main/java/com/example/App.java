4b941dc7a8727b70d611a87fec51e27a39e878e1
waiaan <waiaan@163.com>
2021-08-02 10:26:42 +0800

    update

--- springboot/src/main/java/com/example/App.java
+++ springboot/src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Hello world!
@@ -10,6 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        ConfigurableApplicationContext cac = SpringApplication.run(App.class, args);
+        String[] names = cac.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
