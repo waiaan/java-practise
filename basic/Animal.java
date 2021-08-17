63c52b7d7b4dddc2698101e7d760e6907adbe21b
waiaan <waiaan@163.com>
2021-08-18 07:21:53 +0800

    update

--- basic/Animal.java
+++
@@ -1,9 +0,0 @@
-public abstract class Animal {
-    String name = "animal";
-
-    abstract void eat();
-
-    void sleep() {
-        System.out.println("animal sleep");
-    }
-}
--- basic/Test.java
+++ basic/Test.java
@@ -1,15 +1,6 @@
 
-class Test1 {
-    static void eat(Animal a) {
-        a.eat();
-        a.sleep();
-    }
-}
-
 public class Test {
     public static void main(String[] args) {
-        Cat a = new Cat();
-        Cat c = (Cat) a;
-        Test1.eat(c);
+
     }
 }
